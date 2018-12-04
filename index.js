2a765d75d6f1a8d60f850ff117016520f4be8158
Remon Nashid <remon.sherin@gmail.com>
2018-12-04 16:56:55 -0500

    Transpite to es5

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-function get(contract = '', network = '') {
-  contract = contract.toLowerCase()
-  network  = network.toLowerCase()
+'use strict';
+
+function get() {
+  var contract = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+  var network = arguments.length <= 1 || arguments[1] === undefined ? '' : arguments[1];
+
+  contract = contract.toLowerCase();
+  network = network.toLowerCase();
   try {
-    let res = require(`./contracts/${contract}.json`)
-    return res[network]
-  }
-  catch (error) {
-    throw new Error(`Cannot locate contract ${contract} on ${network}: ` + JSON.stringify(error))
+    var res = require('./contracts/' + contract + '.json');
+    return res[network];
+  } catch (error) {
+    throw new Error('Cannot locate contract ' + contract + ' on ' + network + ': ' + JSON.stringify(error));
   }
 }
 
-module.exports = { get }
+module.exports = { get: get };
